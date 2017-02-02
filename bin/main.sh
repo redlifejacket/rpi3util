57b179f24f8533749460e9cafe4b4184d5df3e7b
Shyam Govardhan <shyamg@google.com>
2017-02-02 21:03:01 +1100

    main.sh fixed path bug

--- bin/main.sh
+++ bin/main.sh
@@ -6,7 +6,7 @@ runtime=$(date +%Y%m%d%H%M%S)
 tarfile_before=/tmp/etc_before_${runtime}.tar
 tarfile_after=/tmp/etc_after_${runtime}.tar
 tar cvf $tarfile_before $(for f in `find etc`; do echo "/${f}"; done)
-tar cvf $tarfile_after ../etc
+tar cvf $tarfile_after etc
 
 $projdir/bin/rpi3_ap_setup.sh
 $projdir/bin/adapter_passthrough.sh wlan1 eth0
