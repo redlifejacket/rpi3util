4c7507b185afcb9d00ae19230dfcb603b75e46e4
Shyam Govardhan <shyamg@google.com>
2017-02-02 20:56:45 +1100

    moved main.sh

--- bin/main.sh
+++ bin/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+projdir=/home/pi/rpi3util_master
+cd $projdir
 runtime=$(date +%Y%m%d%H%M%S)
 tarfile_before=/tmp/etc_before_${runtime}.tar
 tarfile_after=/tmp/etc_after_${runtime}.tar
@@ -7,8 +9,8 @@ tar cvf $tarfile_before $(for f in `find etc`; do echo "/${f}"; done)
 tar cvf $tarfile_after ../etc
 tar xvf $tarfile_after -C /
 
-../bin/rpi3_ap_setup.sh
-../bin/adapter_passthrough.sh
-../bin/postfix_main.sh
-../bin/postfix_aliases.sh
-../bin/postfix_test.sh
+$projdir/bin/rpi3_ap_setup.sh
+$projdir/bin/adapter_passthrough.sh
+$projdir/bin/postfix_main.sh
+$projdir/bin/postfix_aliases.sh
+$projdir/bin/postfix_test.sh
