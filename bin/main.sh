b0722e5d25547626567337e8cec3e5e3cf547282
Shyam Govardhan <shyamg@google.com>
2017-02-02 21:01:28 +1100

    adapter_passthrough.sh wlan1 eth0

--- bin/main.sh
+++ bin/main.sh
@@ -7,10 +7,10 @@ tarfile_before=/tmp/etc_before_${runtime}.tar
 tarfile_after=/tmp/etc_after_${runtime}.tar
 tar cvf $tarfile_before $(for f in `find etc`; do echo "/${f}"; done)
 tar cvf $tarfile_after ../etc
-tar xvf $tarfile_after -C /
 
 $projdir/bin/rpi3_ap_setup.sh
-$projdir/bin/adapter_passthrough.sh
+$projdir/bin/adapter_passthrough.sh wlan1 eth0
+tar xvf $tarfile_after -C /
 $projdir/bin/postfix_main.sh
 $projdir/bin/postfix_aliases.sh
 $projdir/bin/postfix_test.sh
