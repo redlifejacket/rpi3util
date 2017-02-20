ec7550ab2c83d814239822a54fcebd2322700a09
Shyam Govardhan <shyamg@google.com>
2017-02-20 19:48:05 +1100

    cluster_idpub cleanup

--- bin/cluster_idpub.sh
+++ bin/cluster_idpub.sh
@@ -3,11 +3,20 @@
 iplist=$(grep rpi /etc/dhcp.machines | awk -F',' '{print $3}')
 user=pirate
 homedir=/home/${user}
+sshdir=${homedir}/.ssh
+idpub=${sshdir}/id_rsa.pub
+authkeys=${sshdir}/authorized_keys
+endpoint=${user}@$(hostname)
 
 echo "iplist: ${iplist}"
 
+if [ ! -f ${idpub} ]
+then
+  ssh-keygen -t rsa -c "${endpoint}"
+fi
 for ip in ${iplist}
 do
-  scp ${homedir}/.ssh/id_rsa.pub pirate@${ip}:${homedir}/.ssh/authorized_keys
+  #scp ${idpub} pirate@${ip}:${homedir}/.ssh/authorized_keys
+  cat ${idpub} | ssh ${endpoint} 'cat >> ${authkeys}'
 done
 
